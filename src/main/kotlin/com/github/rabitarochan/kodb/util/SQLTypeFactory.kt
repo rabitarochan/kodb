d26649a25bac0e99debfad74b9bbebb19dfd8416
rabitarochan <rabitarochan@gmail.com>
2016-02-27 18:16:39 +0900

    Move factory function to main

---
+++ src/main/kotlin/com/github/rabitarochan/kodb/util/SQLTypeFactory.kt
@@ -0,0 +1,35 @@
+package com.github.rabitarochan.kodb.util
+
+import java.util.*
+
+fun sqlDateOf(year: Int, month: Int, date: Int): java.sql.Date {
+    dateOf(year, month, date, 0, 0, 0, 0).let {
+        return java.sql.Date(it.time)
+    }
+}
+
+fun sqlTimeOf(hour: Int, minute: Int, second: Int): java.sql.Time {
+    dateOf(1970, Calendar.JANUARY, 1, hour, minute, second, 0).let {
+        return java.sql.Time(it.time)
+    }
+}
+
+fun sqlTimestampOf(year: Int, month: Int, date: Int, hour: Int, minute: Int, second: Int, milliSecond: Int): java.sql.Timestamp {
+    dateOf(year, month, date, hour, minute, second, milliSecond).let {
+        return java.sql.Timestamp(it.time)
+    }
+}
+
+fun dateOf(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int, milliSecond: Int = 0): java.util.Date {
+    Calendar.getInstance().let {
+        it.time = java.util.Date()
+        it.set(Calendar.YEAR, year)
+        it.set(Calendar.MONTH, month - 1)
+        it.set(Calendar.DATE, day)
+        it.set(Calendar.HOUR_OF_DAY, hour)
+        it.set(Calendar.MINUTE, minute)
+        it.set(Calendar.SECOND, second)
+        it.set(Calendar.MILLISECOND, milliSecond)
+        return it.time
+    }
+}
--- src/test/kotlin/com/github/rabitarochan/kodb/extractor/DefaultExtractorTest.kt
+++ src/test/kotlin/com/github/rabitarochan/kodb/extractor/DefaultExtractorTest.kt
@@ -2,11 +2,14 @@ package com.github.rabitarochan.kodb.extractor
 
 import com.github.rabitarochan.kodb.Session
 import com.github.rabitarochan.kodb.use
+import com.github.rabitarochan.kodb.util.dateOf
+import com.github.rabitarochan.kodb.util.sqlDateOf
+import com.github.rabitarochan.kodb.util.sqlTimeOf
+import com.github.rabitarochan.kodb.util.sqlTimestampOf
 import org.junit.Test
 import java.math.BigDecimal
 import java.sql.Connection
 import java.sql.DriverManager
-import java.util.*
 import kotlin.test.assertEquals
 
 class DefaultExtractorTest {
@@ -256,7 +259,6 @@ class DefaultExtractorTest {
             assertEquals(dateOf(1970, 1, 1, 12, 34, 1, 1).time, xs.first().time)
             assertEquals(dateOf(1970, 1, 10, 12, 34, 10, 10).time, xs.last().time)
         }
-
     }
 
 
@@ -306,38 +308,6 @@ class DefaultExtractorTest {
                             sqlTimestampOf(1970, 1, i, 12, 34, i, i)
                     )
             )
-        }
-    }
-
-    private fun sqlDateOf(year: Int, month: Int, date: Int): java.sql.Date {
-        dateOf(year, month, date, 0, 0, 0, 0).let {
-            return java.sql.Date(it.time)
-        }
-    }
-
-    private fun sqlTimeOf(hour: Int, minute: Int, second: Int): java.sql.Time {
-        dateOf(1970, Calendar.JANUARY, 1, hour, minute, second, 0).let {
-            return java.sql.Time(it.time)
-        }
-    }
-
-    private fun sqlTimestampOf(year: Int, month: Int, date: Int, hour: Int, minute: Int, second: Int, milliSecond: Int): java.sql.Timestamp {
-        dateOf(year, month, date, hour, minute, second, milliSecond).let {
-            return java.sql.Timestamp(it.time)
-        }
-    }
-
-    private fun dateOf(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int, milliSecond: Int = 0): java.util.Date {
-        Calendar.getInstance().let {
-            it.time = java.util.Date()
-            it.set(Calendar.YEAR, year)
-            it.set(Calendar.MONTH, month - 1)
-            it.set(Calendar.DATE, day)
-            it.set(Calendar.HOUR_OF_DAY, hour)
-            it.set(Calendar.MINUTE, minute)
-            it.set(Calendar.SECOND, second)
-            it.set(Calendar.MILLISECOND, milliSecond)
-            return it.time
         }
     }
 
